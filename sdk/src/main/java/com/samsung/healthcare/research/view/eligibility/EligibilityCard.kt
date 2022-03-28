c7375da941384bc3c79a09376074d8799c3f4819
곽창근/Data Research팀(SR)/삼성전자 <culater.kwak@samsung.com>
2022-03-28 10:19:19 +0900

    :sparkles: Create EligibilityOverviewTab (#45)
    * :sparkles: Create EligibilityOverviewTab
    
    Signed-off-by: culater.kwak <culater.kwak@samsung.com>
    
    * fix ktlintcheck error

--- sdk/src/main/java/com/samsung/healthcare/research/view/eligibility/EligibilityCard.kt
+++ sdk/src/main/java/com/samsung/healthcare/research/view/eligibility/EligibilityCard.kt
@@ -5,40 +5,116 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Tab
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import com.google.accompanist.pager.rememberPagerState
 import com.samsung.healthcare.research.R
 import com.samsung.healthcare.research.model.eligibility.EligibilityOverviewContent
 import com.samsung.healthcare.research.theme.AppTheme
 import com.samsung.healthcare.research.view.common.SdkCard
+import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun EligibilityOverviewTab(
+    modifier: Modifier = Modifier,
+    unselectedTabIcon: Int = R.drawable.tab_icon_unselected,
+    selectedTabIcon: Int = R.drawable.tab_icon_selected,
+    contents: List<EligibilityOverviewContent>,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 50.dp),
+    startScale: Float = 1f,
+    startAlpha: Float = 1f,
+) {
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = modifier,
+    ) {
+        EligibilityOverviewCards(
+            pagerState = pagerState,
+            contents = contents,
+            contentPadding = contentPadding,
+            startScale = startScale,
+            startAlpha = startAlpha,
+        )
+        Row(
+            modifier = Modifier
+                .wrapContentSize()
+                .align(Alignment.CenterHorizontally),
+        ) {
+            contents.forEachIndexed { index, _ ->
+                Surface(
+                    modifier = Modifier.width(20.dp),
+                ) {
+                    val selected = pagerState.currentPage == index
+                    Tab(
+                        modifier = Modifier.wrapContentSize(),
+                        selected = selected,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(
+                                    page = index,
+                                )
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(
+                                    id =
+                                    if (selected) {
+                                        selectedTabIcon
+                                    } else {
+                                        unselectedTabIcon
+                                    }
+                                ),
+                                contentDescription = null,
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun EligibilityOverviewCards(
     modifier: Modifier = Modifier,
+    pagerState: PagerState = rememberPagerState(),
     contents: List<EligibilityOverviewContent>,
     contentPadding: PaddingValues = PaddingValues(horizontal = 50.dp),
     startScale: Float = 1f,
@@ -46,8 +122,9 @@ fun EligibilityOverviewCards(
 ) {
     HorizontalPager(
         count = contents.size,
+        state = pagerState,
         contentPadding = contentPadding,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxWidth()
     ) { page ->
         EligibilityOverviewCard(
             modifier = Modifier.graphicsLayer {
