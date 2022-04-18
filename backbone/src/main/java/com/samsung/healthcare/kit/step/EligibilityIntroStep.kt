8c05513ef64a09b7f828aa24e3b59847d60d1699
김종문/Data Research팀(SR)/삼성전자 <jlego229.kim@samsung.com>
2022-04-18 19:14:20 +0900

    ⚡ Refactor EligibilityIntro Classes using abstract classes (#70)
    * :zap: Refactor EligibilityIntro Classes using abstract classes
    
    * :zap: Refactor EligibilityIntro Classes using abstract classes
    
    Co-authored-by: jlego229-kim <jlego229.kim@samsung.net>

---
+++ backbone/src/main/java/com/samsung/healthcare/kit/step/EligibilityIntroStep.kt
@@ -0,0 +1,18 @@
+package com.samsung.healthcare.kit.step
+
+import androidx.compose.runtime.Composable
+import com.samsung.healthcare.kit.common.CallbackCollection
+import com.samsung.healthcare.kit.model.EligibilityIntroModel
+import com.samsung.healthcare.kit.view.EligibilityIntroView
+
+class EligibilityIntroStep(
+    id: String,
+    name: String,
+    model: EligibilityIntroModel,
+    view: EligibilityIntroView,
+) : Step<EligibilityIntroModel, Boolean>(id, name, model, view, getResult = { true }) {
+
+    @Composable
+    override fun Render(callbackCollection: CallbackCollection): Unit =
+        view.Render(model, callbackCollection, null)
+}
