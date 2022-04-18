04041799683abca48b5aa5265b23e53df10641a4
김종문/Data Research팀(SR)/삼성전자 <jlego229.kim@samsung.com>
2022-04-18 21:13:04 +0900

    ⚡ Refactor EligibilityChecker Classes using abstract classes (#71)
    * Refactor EligibilityChecker Classes using abstract classes
    
    * Update backbone/src/main/java/com/samsung/healthcare/kit/step/EligibilityCheckerStep.kt
    
    Co-authored-by: 도지윤/Data Research팀(SR)/삼성전자 <jjyun.do@samsung.com>
    
    * Update backbone/src/main/java/com/samsung/healthcare/kit/view/EligibilityCheckerView.kt
    
    Co-authored-by: 도지윤/Data Research팀(SR)/삼성전자 <jjyun.do@samsung.com>
    
    Co-authored-by: jlego229-kim <jlego229.kim@samsung.net>
    Co-authored-by: 도지윤/Data Research팀(SR)/삼성전자 <jjyun.do@samsung.com>

---
+++ backbone/src/main/java/com/samsung/healthcare/kit/step/EligibilityCheckerStep.kt
@@ -0,0 +1,20 @@
+package com.samsung.healthcare.kit.step
+
+import androidx.compose.runtime.Composable
+import com.samsung.healthcare.kit.common.CallbackCollection
+import com.samsung.healthcare.kit.model.EligibilityCheckerModel
+import com.samsung.healthcare.kit.step.sub.SubStepHolder
+import com.samsung.healthcare.kit.view.EligibilityCheckerView
+
+class EligibilityCheckerStep(
+    id: String,
+    name: String,
+    model: EligibilityCheckerModel,
+    view: EligibilityCheckerView,
+    val subStepHolder: SubStepHolder,
+) : Step<EligibilityCheckerModel, Boolean>(id, name, model, view, { true }) {
+
+    @Composable
+    override fun Render(callbackCollection: CallbackCollection): Unit =
+        view.Render(model, callbackCollection, subStepHolder)
+}
